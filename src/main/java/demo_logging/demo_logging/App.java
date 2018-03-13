b5ed2627d579c7bae3b2a68767bc6cf94f9abad9
victorahs <victorahsene90@gmail.com>
2018-03-13 11:14:40 +0100

    Initialize

---
+++ src/main/java/demo_logging/demo_logging/App.java
@@ -0,0 +1,13 @@
+package demo_logging.demo_logging;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/demo_logging/demo_logging/dev/App.java
@@ -0,0 +1,17 @@
+package demo_logging.demo_logging.dev;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dev.service.AppService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class App {
+private static final Logger LOG = LoggerFactory.getLogger(App.class);
+public static void main(String[] args) {
+LOG.info("Bonjour Slf4J !");
+LOG.info("Implémentation Logback");
+new AppService().executer("valeur 1"); 
+
+}
+}
